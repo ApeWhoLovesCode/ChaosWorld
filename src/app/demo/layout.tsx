01cc872d7003a60e2d1c50c78aa6b35694a982ad
linhuanheng <linhuanheng@vzan.com>
2025-07-21 20:40:23 +0800

    fix: 完善布局

---
+++ src/app/demo/layout.tsx
@@ -0,0 +1,13 @@
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="border-2 border-solid border-zinc-600 p-4 dark:border-zinc-400 h-[90%] aspect-square rounded-lg">
+        {children}
+      </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="zh-cn" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
